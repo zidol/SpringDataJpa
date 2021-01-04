588ab1d9f0173aa24af5dc8ca8ffd997a67039f1
zidol <ajh3166@naver.com>
2021-01-05 00:28:36 +0900

    design example domain

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,43 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)  //xToOne : LAZY 모드
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    //jpa 는 기본적으로 디폴트 생성자를 선언 해줘야함 -> lombok :@NoArgsConstructor(access = AccessLevel.PROTECTED) 해결 가능
+//    protected Member() {
+//    }
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
